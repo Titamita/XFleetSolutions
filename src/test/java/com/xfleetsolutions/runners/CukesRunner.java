a27b9fa995d2543e27b387cf0eb35d4d819cc5cc
Titamita <juliadancs@gmail.com>
2023-10-30 15:03:33 -0400

    Initial Project Setup

---
+++ src/test/java/com/xfleetsolutions/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.xfleetsolutions.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+        },
+        features = "src/test/resources/features",
+        glue = "com/xfleetsolutions/step_definitions",
+        dryRun = false,
+        tags = " @wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
---
+++ src/test/java/com/xfleetsolutions/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.xfleetsolutions.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/xfleetsolutions/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
