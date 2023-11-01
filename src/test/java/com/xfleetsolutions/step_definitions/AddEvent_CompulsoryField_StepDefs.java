ec27729d79d86f2be46d97afaafe649151e05ede
Kateryna <katemart17@gmail.com>
2023-11-01 17:06:45 -0400

    Add step definitions for AC2 Scenario

--- src/test/java/com/xfleetsolutions/step_definitions/AddEvent_CompulsoryField_StepDefs.java
+++ src/test/java/com/xfleetsolutions/step_definitions/AddEvent_CompulsoryField_StepDefs.java
@@ -3,6 +3,7 @@ package com.xfleetsolutions.step_definitions;
 import com.xfleetsolutions.pages.AddEventPage;
 import com.xfleetsolutions.pages.BasePage;
 import com.xfleetsolutions.pages.FleetVehiclesPage;
+import com.xfleetsolutions.utilities.BrowserUtils;
 import com.xfleetsolutions.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -32,7 +33,6 @@ public class AddEvent_CompulsoryField_StepDefs extends BasePage {
 
     @When("click on vehicle row")
     public void click_on_vehicle_row() {
-
         fp.vehicleRow.click();
 
     }
@@ -41,6 +41,19 @@ public class AddEvent_CompulsoryField_StepDefs extends BasePage {
     public void user_should_access_the_add_event_page() {
 
         Assert.assertTrue(ae.addEventButton.isDisplayed());
+
+    }
+
+    @Then("{string} can click Add Event button, but driver can NOT see Add Event button")
+    public void can_click_add_event_button_but_driver_can_not_see_add_event_button(String user) {
+       BrowserUtils.sleep(3);
+        if (user.equals("sales manager") || user.equals("store manager")){
+            Assert.assertTrue(ae.addEventButton.isDisplayed());
+        }
+
+        if (user.equals("driver")){
+            Assert.assertFalse(ae.addEventButton.isDisplayed());
+        }
 
     }
 
