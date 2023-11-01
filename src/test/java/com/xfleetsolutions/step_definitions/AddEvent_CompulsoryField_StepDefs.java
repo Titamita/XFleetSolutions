a5323ac20df628f993c6473e2a8f58afc83bfadb
Kateryna <katemart17@gmail.com>
2023-11-01 19:26:35 -0400

    Create snippets and write code for AC3 in StepDefs

--- src/test/java/com/xfleetsolutions/step_definitions/AddEvent_CompulsoryField_StepDefs.java
+++ src/test/java/com/xfleetsolutions/step_definitions/AddEvent_CompulsoryField_StepDefs.java
@@ -20,10 +20,11 @@ public class AddEvent_CompulsoryField_StepDefs extends BasePage {
     AddEventPage ae = new AddEventPage();
     Actions actions = new Actions(Driver.getDriver());
 
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+
     @When("user hover over on Fleet button and click Vehicle button")
     public void user_hover_over_on_fleet_button_and_click_vehicle_button() {
 
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(fleetButton));
 
         actions.moveToElement(fleetButton).perform();
@@ -33,6 +34,7 @@ public class AddEvent_CompulsoryField_StepDefs extends BasePage {
 
     @When("click on vehicle row")
     public void click_on_vehicle_row() {
+
         fp.vehicleRow.click();
 
     }
@@ -40,20 +42,35 @@ public class AddEvent_CompulsoryField_StepDefs extends BasePage {
     @Then("user should access the Add Event page")
     public void user_should_access_the_add_event_page() {
 
-        Assert.assertTrue(ae.addEventButton.isDisplayed());
+        Assert.assertTrue(ae.generalInfo.isDisplayed());
 
     }
 
     @Then("{string} can click Add Event button, but driver can NOT see Add Event button")
     public void can_click_add_event_button_but_driver_can_not_see_add_event_button(String user) {
-       BrowserUtils.sleep(3);
-        if (user.equals("sales manager") || user.equals("store manager")){
+        BrowserUtils.sleep(3);
+        if (user.equals("sales manager") || user.equals("store manager")) {
             Assert.assertTrue(ae.addEventButton.isDisplayed());
         }
 
-        if (user.equals("driver")){
+        if (user.equals("driver")) {
             Assert.assertFalse(ae.addEventButton.isDisplayed());
         }
+
+    }
+
+    @When("{string} click Add Event button")
+    public void click_add_event_button(String user) {
+
+        ae.addEventButton.click();
+
+    }
+
+    @Then("Add Event page should pop up")
+    public void add_event_page_should_pop_up() {
+
+        wait.until(ExpectedConditions.visibilityOf(ae.addEventPopUp));
+        Assert.assertTrue(ae.addEventPopUp.isDisplayed());
 
     }
 
