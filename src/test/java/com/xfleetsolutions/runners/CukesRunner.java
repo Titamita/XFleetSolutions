f3fff8a1d2d03e7b82f15ea2ec6ea8be7df405f1
artiomkisliakov <artiom.kis@gmail.com>
2023-11-08 09:37:40 -0500

    I just used for getting step. def. snippets

--- src/test/java/com/xfleetsolutions/runners/CukesRunner.java
+++ src/test/java/com/xfleetsolutions/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/xfleetsolutions/step_definitions",
         dryRun = false,
-        tags = " @wip",
+        tags = "@carDeletion",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
